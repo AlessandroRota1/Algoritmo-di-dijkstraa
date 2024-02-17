69e4d5717abd0b3746aa4a70bfc753b9d3d1387c
Rota.20073 <rotaale05102006@gmail.com>
2024-02-17 10:16:34 +0100

    Implementazione calcolo distanza

--- Algoritmo di dijkstraa/Program.cs
+++ Algoritmo di dijkstraa/Program.cs
@@ -3,67 +3,111 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Algoritmo_di_dijkstraa
 {
-    class Program
+    internal class Program
     {
-        static int[,] GeneraMatriceSpecchiataConZeroObliqui(int righe, int colonne)
+        class Dijkstra
+        {
+            public static void EseguiDijkstra(int[,] grafo, int numeroNodi)
+            {
+                int[] distanza = new int[numeroNodi];
+                int[] precedente = new int[numeroNodi];
+                bool[] visitato = new bool[numeroNodi];
+                for (int i = 0; i < numeroNodi; i++)
+                {
+                    distanza[i] = int.MaxValue;
+                    precedente[i] = -1;
+                    visitato[i] = false;
+                }
+                distanza[0] = 0;
+                for (int count = 0; count < numeroNodi - 1; count++)
+                {
+                    int u = TrovaMinimaDistanza(distanza, visitato);
+                    visitato[u] = true;
+                    for (int v = 0; v < numeroNodi; v++)
+                    {
+                        if (!visitato[v] && grafo[u, v] > 0)
+                        {
+                            int nuovaDistanza = distanza[u] + grafo[u, v];
+                            if (nuovaDistanza < distanza[v])
+                            {
+                                distanza[v] = nuovaDistanza;
+                                precedente[v] = u;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine("Distanze minime dal nodo iniziale:");
+                for (int i = 0; i < numeroNodi; i++)
+                {
+                    Console.WriteLine($"{(char)(65 + i)}: {distanza[i]}");
+                }
+            }
+            private static int TrovaMinimaDistanza(int[] distanza, bool[] visitato)
+            {
+                int minimaDistanza = int.MaxValue;
+                int minimoNodo = -1;
+                for (int nodo = 0; nodo < distanza.Length; nodo++)
+                {
+                    if (!visitato[nodo] && distanza[nodo] < minimaDistanza)
+                    {
+                        minimaDistanza = distanza[nodo];
+                        minimoNodo = nodo;
+                    }
+                }
+                return minimoNodo;
+            }
+        }
+        static void Main(string[] args)
         {
             Random rnd = new Random();
-            int valoreCasuale = 0;
-            int[,] matrice = new int[righe, colonne];
-
+            int numeroNodi = rnd.Next(3, 10);
+            int righe = numeroNodi;
+            int colonne = numeroNodi;
+            int[,] matrice;
+            int ascii = 65;
+            Console.WriteLine($"Il numero di nodi è {numeroNodi}");
+            matrice = GeneratoreMatrice(righe, colonne);
+            Console.WriteLine($"Il peso dei nodi è:");
+            Console.Write("\n \t");
             for (int i = 0; i < righe; i++)
             {
-                for (int j = 0; j < colonne / 2 + 1; j++)
+                Console.Write($"{(char)(ascii + i)}\t");
+            }
+            for (int i = 0; i < righe; i++)
+            {
+                Console.WriteLine("\n");
+                Console.Write($"{(char)(ascii + i)}\t");
+                for (int j = 0; j < colonne; j++)
+                {
+                    Console.Write($"{matrice[i, j]} \t");
+                }
+            }
+            Dijkstra.EseguiDijkstra(matrice, numeroNodi);
+            Console.ReadKey();
+        }
+        static int[,] GeneratoreMatrice(int righe, int colonne)
+        {
+            Random rnd = new Random();
+            int[,] matrice = new int[righe, colonne];
+            for (int i = 0; i < righe; i++)
+            {
+                for (int j = 0; j < colonne; j++)
                 {
                     do
                     {
-                        valoreCasuale = rnd.Next(-1, 11);
+                        matrice[i, j] = rnd.Next(-1, 9);
                     }
-                    while (valoreCasuale == 0);
-                    matrice[i, j] = valoreCasuale;
-                    matrice[j, i] = valoreCasuale;
+                    while (matrice[i, j] == 0);
                 }
                 matrice[i, i] = 0;
             }
             return matrice;
         }
-        public static void Main()
-        {
-            Console.WriteLine("Inserisci nodo di partenza");
-            string NodoPartenza = Console.ReadLine();
-            Console.WriteLine("Inserisci nodo di arrivo");
-            string NodoArrivo = Console.ReadLine();
-            Random random = new Random();
-            int[,] matrice = GeneraMatriceSpecchiataConZeroObliqui(4, 4);
-            for (int i = 0; i < matrice.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrice.GetLength(1); j++)
-                {
-                    Console.Write(matrice[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("La distanza è ") ;
-            if (NodoPartenza =="A")
-            {
-                
-            }
-            if (NodoPartenza == "B")
-            {
-                
-            }
-            if (NodoPartenza == "C")
-            {
-                
-            }
-            if (NodoPartenza == "D")
-            { 
-                
-            }
-        }
-
     }
 }
